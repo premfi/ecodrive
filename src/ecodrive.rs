bf97684013e2f28421d677650e14361b215475b6
premfi <finn.prem@gmx.de>
2026-06-05 15:07:43 +0200

    Define Vehicle, Route and constants

---
+++ src/ecodrive.rs
@@ -0,0 +1,86 @@
+pub type PrefFloat = f64; // preferred floating point type
+
+use uom::si::f64::{Mass, Area, Acceleration, MassDensity, Length, Velocity, Ratio};
+use uom::typenum::{N1, Z0};
+use std::marker::PhantomData;
+
+pub const GRAVITY_OF_EARTH: Acceleration = Acceleration {dimension:PhantomData, units: PhantomData, value: 9.81}; // gravitational acceleration [m/s^2]
+// pub const C_R: PrefFloat = 0.01; // rolling resistance coefficient
+// pub const MASS: Mass = Mass {dimension: PhantomData, units: PhantomData, value: 2000.0}; // vehicle mass [kg]
+// pub const RHO_ROT: PrefFloat = 1.1; // factor for equivalent mass of rotating parts
+pub const RHO_AIR: MassDensity = MassDensity {dimension: PhantomData, units: PhantomData, value: 1.2}; // air density [kg/m^3]
+// pub const C_W: PrefFloat = 0.3;     // drag coefficient
+// pub const A_FRONT: Area = Area {dimension: PhantomData, units: PhantomData, value: 2.0}; // frontal area [m^2]
+// pub const C_DEFAULT: PrefFloat = RHO_AIR * C_W * A_FRONT / MASS;
+// pub const REC_EFF: PrefFloat = 0.3; // regenerative braking efficiency
+
+pub struct Vehicle {
+    pub roll_res_coeff: PrefFloat,  // rolling resistance coefficient
+    pub mass: Mass,                 // vehicle mass [kg]
+    pub rho_rot: PrefFloat,         // factor for equivalent mass of rotating parts
+    pub c_w: PrefFloat,             // drag coefficient
+    pub frontal_area: Area,         // frontal area [m^2]
+    pub rec_eff: PrefFloat,          // regenerative braking efficiency
+    c_param: Option<uom::si::Quantity<uom::si::ISQ<N1, Z0, Z0, Z0, Z0, Z0, Z0>,
+                                        uom::si::SI<PrefFloat>, PrefFloat>> // C parameter (mass-normalized air resistance prefactor) [1/m]. Automatically calculated.
+}
+
+impl Vehicle {
+    pub fn builder(roll_res_coeff: PrefFloat, // rolling resistance coefficient
+                mass: Mass,                 // vehicle mass [kg]
+                rho_rot: PrefFloat,         // factor for equivalent mass of rotating parts
+                c_w: PrefFloat,             // drag coefficient
+                frontal_area: Area,         // frontal area [m^2]
+                rec_eff: PrefFloat     // regenerative braking efficiency
+                ) -> Vehicle {
+        // fill the public fields
+        let mut vhl = Vehicle {roll_res_coeff,
+                    mass,
+                    rho_rot,
+                    c_w,
+                    frontal_area,
+                    rec_eff,
+                    c_param: None};
+
+        // calculate and set c_param from given values
+        vhl.c_param = Some(vhl.calc_c_param());
+
+        vhl
+    }
+
+    fn calc_c_param(&self) -> uom::si::Quantity<uom::si::ISQ<N1, Z0, Z0, Z0, Z0, Z0, Z0>,
+                                            uom::si::SI<PrefFloat>, PrefFloat> /* [1/m] */ {
+        /* calculate c_param from given values */
+        RHO_AIR * self.c_w * self.frontal_area / self.mass
+    }
+
+    pub fn get_c_param(&self) -> uom::si::Quantity<uom::si::ISQ<N1, Z0, Z0, Z0, Z0, Z0, Z0>,
+                                        uom::si::SI<PrefFloat>, PrefFloat> /* [1/m] */ {
+        /* get value from private c_param field */
+        self.c_param.expect("c_param not set! Should have been calculated automatically.")
+    }
+}
+
+
+pub struct Route {
+    pub lengths: Vec<Length>,
+    pub slopes: Vec<Ratio>,
+    pub max_speeds: Vec<Velocity>, 
+
+}
+
+// fn e_kin(s, )
+
+// fn energy_used(s, mom)
+
+// fn retrieve_A(s, )
+
+// fn delta_t(s, A, C, e_kin0)
+
+// fn v_to_ekin(v)
+
+// fn ekin_to_v(ekin)
+
+// *discretize and undiscretice t and v (4 functions in total)*
+
+// fn DP(route, vehicle, max_time, time_res, v_res)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+mod ecodrive;
+use ecodrive::*;
+
+use uom::si::f64::{Mass, Area, Length, Ratio, Velocity};
+use uom::si::{mass::kilogram, area::square_meter, length::meter, ratio::percent, velocity::{meter_per_second, kilometer_per_hour}};
+use uom;
+
+
 fn main() {
+    /* TODO: Instead of manually defining vehicles, they may be imported from a .csv file, stored in a list, and then executed one by one */
+    let car1 = Vehicle::builder(0.01,
+                                Mass::new::<kilogram>(2000.0),
+                                1.1,
+                                0.3,
+                                Area::new::<square_meter>(2.0),
+                                0.3);
+
+    let lengths = vec![Length::new::<meter>(50.0),
+                        Length::new::<meter>(100.0),
+                        Length::new::<meter>(100.0),
+                        Length::new::<meter>(50.0),];
+
+    let slopes = vec![Ratio::new::<percent>(0.0),
+                        Ratio::new::<percent>(5.0),
+                        Ratio::new::<percent>(-5.0),
+                        Ratio::new::<percent>(0.0)];
+
+    let max_speeds = vec![Velocity::new::<meter_per_second>(100.0 / 3.6),
+                        Velocity::new::<kilometer_per_hour>(100.0), // is automatically converted to m/s
+                        Velocity::new::<meter_per_second>(130.0 / 3.6),
+                        Velocity::new::<meter_per_second>(130.0 / 3.6)];
+    
+    println!("{:?}", max_speeds[0]);
+    println!("{:?}", max_speeds[1]);
+    
+    let route0 = Route {lengths, slopes, max_speeds};
+
     println!("Hello, world!");
+    println!("g={:?}", GRAVITY_OF_EARTH);
+    println!("{:?}", car1.mass * car1.frontal_area);
+    let g = uom::si::acceleration::meter_per_second_squared{};
+    println!("c={:?}", car1.get_c_param());
+    // println!("c={:?}", car1.calc_c_param());
+    println!("{:?}", g);
 }
